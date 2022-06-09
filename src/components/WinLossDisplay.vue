0e4af0d0f7d63eb1453f9a4079a0721ebb878959
Vann <maka3810@gmail.com>
2022-06-10 01:56:00 +0800

    输赢显示 [修正] 负数

--- src/components/WinLossDisplay.vue
+++ src/components/WinLossDisplay.vue
@@ -7,12 +7,16 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component({
     filters: {
         hasPlus(value: string) {
-            let  str = value.replace("$","");
+            let str = value.replace("$", "");
             let amount = Number(str);
             if (amount > 0) {
                 return `+$${amount.toFixed(3)}`;
+            } else if (amount < 0) {
+                amount = Math.abs(amount);
+                return `-$${amount.toFixed(3)}`;
+            } else {
+                return `$${amount.toFixed(3)}`;
             }
-            return `$${amount.toFixed(3)}`;
         },
     },
 })
@@ -20,7 +24,7 @@ export default class WinLossDisplay extends Vue {
     @Prop({ type: String, default: "0" }) amount!: string;
 
     private checkWin(value: string) {
-        let str = value.replace("$","");
+        let str = value.replace("$", "");
         let amount = Number(str);
         if (amount > 0) return "green-color";
         else if (amount < 0) return "red-color";
