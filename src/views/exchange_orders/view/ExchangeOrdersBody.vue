ed85437c0b651042ab63a4d0b41d116298afd44f
Vann <maka3810@gmail.com>
2022-06-20 12:03:31 +0800

    用戶兌換訂單 [修正] key值對應

--- src/views/exchange_orders/view/ExchangeOrdersBody.vue
+++ src/views/exchange_orders/view/ExchangeOrdersBody.vue
@@ -349,19 +349,6 @@ export default class ExchangeOrdersBody extends AbstractView {
     getAccessInfo(data: any) {
         const options: any = this.tableColumns.payment_method.options[data.receive_payment_type];
         let infoStr = "";
-        if (data.receive_payment_type == 3) {
-            infoStr += options["address"] + "：";
-            //不懂这里为什么options中定义的是address，在列表中要取account字段
-            infoStr += data.payment_method["account"] + "<br>";
-            console.log("");
-            return infoStr;
-        } else if (data.receive_payment_type == 4 && options) {
-            infoStr += options["address"] + "：";
-            infoStr += data.payment_method["account"] + "<br>";
-            infoStr += options["remark"] + "：";
-            infoStr += data.payment_method["account_name"] + "<br>";
-            return infoStr;
-        }
         if (options) {
             const keys = Object.keys(options);
             for (const key of keys) {
