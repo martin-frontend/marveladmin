2f4ba2d09bea716e0660fd718fc49548564dc294
adam <adam.dong@baiyitech.com.tw>
2022-08-03 11:32:59 +0800

    质押分红 bug

--- src/views/plat/view/PlatBody.vue
+++ src/views/plat/view/PlatBody.vue
@@ -82,7 +82,7 @@
                 <template slot-scope="{ row }">
                     <el-button
                         size="mini"
-                        :type="tableColumns.all_bonus_model_id.options[row.all_bonus_model_id] ? 'success' : 'danger'"
+                        :type="row.all_bonus_model_id ? 'success' : 'danger'"
                         @click="handleAllBonusModel(row)"
                     >
                         <!-- {{
