a94fd21b8bca4e97467244d6e7ba38ebd8aea74e
adam <adam.dong@baiyitech.com.tw>
2022-09-07 13:00:25 +0800

    区号管理

---
+++ src/views/system_sms_area_code/view/SystemSmsAreaCode.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="content">
+        <SystemSmsAreaCodeDialog v-if="myProxy.dialogData.bShow" />
+        <SystemSmsAreaCodeHeader />
+        <SystemSmsAreaCodeBody />
+    </div>
+</template>
+
+<script lang="ts">
+import AbstractView from "../../../core/abstract/AbstractView";
+import SystemSmsAreaCodeMediator from "@/views/system_sms_area_code/mediator/SystemSmsAreaCodeMediator";
+import SystemSmsAreaCodeProxy from "@/views/system_sms_area_code/proxy/SystemSmsAreaCodeProxy";
+import { Component } from "vue-property-decorator";
+import SystemSmsAreaCodeDialog from "./SystemSmsAreaCodeDialog.vue";
+import SystemSmsAreaCodeHeader from "./SystemSmsAreaCodeHeader.vue";
+import SystemSmsAreaCodeBody from "./SystemSmsAreaCodeBody.vue";
+
+@Component({
+    components: {
+        SystemSmsAreaCodeDialog,
+        SystemSmsAreaCodeHeader,
+        SystemSmsAreaCodeBody,
+    },
+})
+export default class SystemSmsAreaCode extends AbstractView {
+    constructor() {
+        super(SystemSmsAreaCodeMediator);
+    }
+
+    destroyed() {
+        super.destroyed();
+    }
+    // proxy
+    private myProxy: SystemSmsAreaCodeProxy = this.getProxy(SystemSmsAreaCodeProxy);
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/common.scss";
+</style>
---
+++ src/views/system_sms_area_code/view/SystemSmsAreaCodeDialog.vue
@@ -0,0 +1,98 @@
+<template>
+    <el-dialog :title="textMap[status]" :visible.sync="myProxy.dialogData.bShow">
+        <el-form ref="form" :rules="rules" :model="form" label-width="115px" v-loading="net_status.loading"
+            ><el-form-item :label="tableColumns.name.name" prop="name">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item :label="tableColumns.area_code.name" prop="area_code">
+                <el-input v-model="form.area_code"></el-input>
+            </el-form-item>
+            <el-form-item :label="tableColumns.icon.name" prop="icon">
+                <el-input v-model="form.icon"></el-input>
+            </el-form-item>
+        </el-form>
+        <div class="btn_group">
+            <el-button type="danger" v-if="isStatusUpdate" @click="handleDelete()">{{ $t("common.delete") }}</el-button>
+            <el-button type="primary" @click="isStatusUpdate ? handleUpdate() : handleAdd()">{{
+                $t("common.save")
+            }}</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script lang="ts">
+import AbstractView from "@/core/abstract/AbstractView";
+import { checkUnique, unique } from "@/core/global/Permission";
+import SystemSmsAreaCodeProxy from "@/views/system_sms_area_code/proxy/SystemSmsAreaCodeProxy";
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { checkUserName, checkUserPassword } from "@/core/global/Functions";
+import { DialogStatus } from "@/core/global/Constant";
+import GlobalVar from "@/core/global/GlobalVar";
+
+@Component
+export default class SystemSmsAreaCodeDialog extends AbstractView {
+    // 权限标识
+    private unique = unique;
+    private checkUnique = checkUnique;
+    //网络状态
+    private net_status = GlobalVar.net_status;
+    // proxy
+    private myProxy: SystemSmsAreaCodeProxy = this.getProxy(SystemSmsAreaCodeProxy);
+    // proxy property
+    private tableColumns = this.myProxy.tableData.columns;
+    private form = this.myProxy.dialogData.form;
+
+    private textMap = {
+        update: "编辑",
+        create: "新增",
+    };
+
+    @Watch("myProxy.dialogData.bShow")
+    private onWatchShow() {
+        this.$nextTick(() => {
+            (this.$refs["form"] as Vue & { clearValidate: () => void }).clearValidate();
+        });
+    }
+
+    get status() {
+        return this.myProxy.dialogData.status;
+    }
+
+    get isStatusUpdate() {
+        return this.status == DialogStatus.update;
+    }
+
+    get rules() {
+        return {};
+    }
+
+    private handleAdd() {
+        (this.$refs["form"] as Vue & { validate: (cb: any) => void }).validate((valid: boolean) => {
+            if (valid) {
+                this.myProxy.onAdd();
+            }
+        });
+    }
+
+    private handleUpdate() {
+        (this.$refs["form"] as Vue & { validate: (cb: any) => void }).validate((valid: boolean) => {
+            if (valid) {
+                this.myProxy.onUpdate();
+            }
+        });
+    }
+
+    private handleDelete() {
+        this.myProxy.onDelete(this.form.id);
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/common.scss";
+.btn_group {
+    display: flex;
+    justify-content: flex-end;
+    position: relative;
+}
+</style>
