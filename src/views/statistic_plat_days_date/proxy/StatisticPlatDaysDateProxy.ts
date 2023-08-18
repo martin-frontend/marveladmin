1e50b12dc186b3897ff1ba232e204745ae24addf
martin <martin@gmail.com>
2023-08-18 08:52:20 +0000

    H5-5062

--- src/views/statistic_plat_days_date/proxy/StatisticPlatDaysDateProxy.ts
+++ src/views/statistic_plat_days_date/proxy/StatisticPlatDaysDateProxy.ts
@@ -27,40 +27,185 @@ export default class StatisticPlatDaysDateProxy extends AbstractProxy implements
     /**表格相关数据 */
     tableData = {
         columns: {
-            active_user: { name: "活跃用户", options: {}, display: true },
-            backwater_gold: { name: "实时返水", options: {}, tips: "玩家在实时返水中获得的金额", display: true },
-            channel_id: { name: "所属渠道", options: {}, display: true },
-            commission_gold: { name: "推广返佣", options: {}, tips: "玩家在推广赚钱中获得的返佣金额", display: true },
-            created_date: { name: "日期", options: {}, display: true },
-            data_belong: { name: "数据归属标记", options: {}, display: true },
-            exchange: { name: "兑换金额", options: {}, display: true },
-            exchange_user: { name: "总兑换人数", options: {}, display: true },
-            gift_gold: { name: "平台赠送", options: {}, tips: "玩家在活动、平台赠送中获得的金额", display: true },
-            id: { name: "平台统计ID", options: {}, display: true },
-            new_exchange_user: { name: "新增兑换人数", options: {}, display: true },
-            new_recharge_user: { name: "新增充值人数", options: {}, display: true },
-            new_register: { name: "新增注册", options: {}, display: true },
+            id: { name: "平台统计ID", options: [], display: true },
+            data_belong: { name: "数据归属标记", options: [], display: true },
+            created_date: { name: "日期", options: [], display: true },
             plat_id: { name: "所属平台", options: {}, display: true },
-            recharge: { name: "充值金额", options: {}, display: true },
-            recharge_user: { name: "充值人数", options: {}, display: true },
-            repeat_recharge_user: { name: "重复充值人数", options: {} },
-            water: { name: "游戏流水", options: {}, display: true },
-            win_loss: { name: "游戏输赢", options: {}, display: true },
-            new_user: { name: "新增登入" },
-            agent_bonus_gold: { name: "", options: {} },
-            new_register_device: { name: "新增注册设备", options: {} },
-            effective_new_rate: { name: "有效新增率", options: {} },
-            new_user_recharge: { name: "新用户充值金额", options: {} },
-            old_user_recharge: { name: "老用户充值金额", options: {} },
-            new_recharge_rate: { name: "新增充值率", options: {} },
-            new_register_water: { name: "新用户游戏流水", options: {} },
-            active_user_week: { name: "周活跃用户", options: {} },
-            active_user_month: { name: "月活跃用户", options: {} },
-            active_recharge_user: { name: "活跃充值人数", options: {} },
-            active_user_recharge: { name: "活跃充值金额", options: {} },
-            recharge_seep_rate: { name: "充值渗透率", options: {} },
-            arpu_rate: { name: "arpu_rate", options: {} },
-            arppu_rate: { name: "arppu_rate", options: {} },
+            channel_id: { name: "所属渠道", options: {}, display: true },
+            new_register: { name: "当天注册人数", options: [], display: true, tips: "当前日期新增注册人数" },
+            active_user: { name: "活跃用户", options: [], display: true, tips: "(DAU)活跃用户" },
+            recharge: {
+                name: "充值金额",
+                options: [],
+                display: true,
+                tips:
+                    "在查询时间范围，平台下所有用户的【状态】为“充值成功”的金额，vip充值金额+vip补单+第三方充值金额+手动补单金额",
+            },
+            recharge_user: {
+                name: "充值人数",
+                options: [],
+                display: true,
+                tips: "平台下 充值成功【状态】的用户人数总人数，包含新老用户的充值人数",
+            },
+            new_recharge_user: {
+                name: "首充人数",
+                options: [],
+                display: true,
+                tips: "第一次充值成功的总人数 ，也就是首充总人数",
+            },
+            repeat_recharge_user: { name: "重复充值人数", options: [], display: true },
+            exchange: {
+                name: "兑换金额",
+                options: [],
+                display: true,
+                tips: "在查询时间范围，平台下所有用户的【状态】为“提款成功”的金额，vip充值通道+普通充值通道",
+            },
+            exchange_user: {
+                name: "总兑换人数",
+                options: [],
+                display: true,
+                tips: "平台下 兑换成功【状态】的用户人数总数",
+            },
+            new_exchange_user: { name: "首兑人数", options: [], display: true },
+            win_loss: { name: "游戏输赢", options: [], display: true, tips: "已结算订单的输赢统计,以平台角度展示" },
+            water: {
+                name: "游戏流水",
+                options: [],
+                display: true,
+                tips: "各种不同游戏类型可以分别设置流水比例, 已结算订单的输赢金额 乘以设置的流水比例 就是游戏流水",
+            },
+            gift_gold: { name: "平台赠送", options: [], display: true, tips: "玩家在活动、平台赠送中获得的金额" },
+            backwater_gold: { name: "游戏挖矿", options: [], display: true, tips: "玩家在游戏挖矿中获得的金额" },
+            agent_bonus_gold: { name: "质押分红", options: [], display: true },
+            commission_gold: { name: "推广赚钱", options: [], display: true, tips: "玩家在推广赚钱中获得的返佣金额" },
+            new_register_device: {
+                name: "当天注册设备",
+                options: [],
+                display: true,
+                tips: "当日新注册且为新设备的用户，机器码为唯一。",
+            },
+            new_user_recharge: {
+                name: "首充金额",
+                options: [],
+                display: true,
+                tips: "平台第一次充值成功的总金额 ，也就是首充总金额",
+            },
+            old_user_recharge: {
+                name: "非首充金额",
+                options: [],
+                display: true,
+                tips: "当日老用户充值成功的金额，非首充的总金额",
+            },
+            new_register_water: {
+                name: "新用户游戏流水",
+                options: [],
+                display: true,
+                tips: "统计当日新增的用户的游戏流水",
+            },
+            active_user_week: {
+                name: "周活跃用户",
+                options: [],
+                display: true,
+                tips: "周活跃人数（是指当前日期七天前数据的累积）",
+            },
+            active_user_month: {
+                name: "月活跃用户",
+                options: [],
+                display: true,
+                tips: "月活跃人数（是指当前日期30天数据的累积）",
+            },
+            active_recharge_user: {
+                name: "活跃充值人数",
+                options: [],
+                display: true,
+                tips: "当日活跃且充值的用户人数",
+            },
+            active_user_recharge: { name: "活跃充值金额", options: [], display: true, tips: "当日活跃人数的充值金额" },
+            effective_new_rate: {
+                name: "有效新增率%",
+                options: [],
+                display: true,
+                tips: "【当天】注册设备/【当天】注册人数",
+            },
+            new_recharge_rate: { name: "新增充值率", options: [], display: true, tips: "新增充值人数/新增注册人数" },
+            recharge_seep_rate: { name: "充值渗透率", options: [], display: true, tips: "活跃充值人数/活跃用户" },
+            arpu_rate: { name: "ARPU", options: [], display: true, tips: "活跃充值金额/活跃用户" },
+            arppu_rate: { name: "ARPPU", options: [], display: true, tips: "活跃充值金额/活跃充值人数" },
+            rech_amt: { name: "人均首存金额", options: [], display: true, tips: "首充金额/首充人数" },
+            new_reg_rech_total: {
+                name: "当天注册充值总金额",
+                options: [],
+                display: true,
+                tips: "是当日注册的用户的充值总金额。比如当日 注册用户A , 他成功充值三次 ， 这个三次的金额都需要统计。",
+            },
+            new_reg_first_rech_count: {
+                name: "当天注册充值人数",
+                options: [],
+                display: true,
+                tips: "是当日注册而且充值成功的用户。这里注意同一个用户需要去重",
+            },
+            exch_amt: {
+                name: "当天注册兑换总金额",
+                options: [],
+                display: true,
+                tips: "是当日注册的用户的兑换总金额。比如当日 注册用户A , 他成功兑换三次 ， 这个三次的金额都需要统计。",
+            },
+            new_exch_count: {
+                name: "当天注册兑换人数",
+                options: [],
+                display: true,
+                tips: "是当日注册而且兑换成功的用户。这里注意同一个用户需要去重",
+            },
+            p_user_rech: { name: "ARPU%(登录用户)", options: [], display: true, tips: "充值金额/注册人数" },
+            p_rech_per_user: {
+                name: "ARPPU%(登录用户)",
+                options: [],
+                display: true,
+                tips: "充值金额/充值人数",
+            },
+            net_rech: { name: "充-兑", options: [], display: true, tips: "充值金额-兑换金额" },
+            dau: { name: "DAU", options: [], display: true, tips: "登录人数(去重)" },
+            new_user_arpu: {
+                name: "新增用户ARPU",
+                options: [],
+                display: true,
+                tips: "当天注册充值总金额/当天注册人数",
+            },
+            new_user_arppu: {
+                name: "新增用户ARPPU",
+                options: [],
+                display: true,
+                tips: "当天注册充值总金额/当天注册充值人数",
+            },
+            d_exch_amt_per_user: {
+                name: "当天人均提现",
+                options: [],
+                display: true,
+                tips: "当天注册兑换总金额/当天注册人数",
+            },
+            d_user_cont_per_user: {
+                name: "当天用户人均贡献",
+                options: [],
+                display: true,
+                tips: "新增用户ARPU-当天人均提现",
+            },
+            d_exch_amt_per_dau: {
+                name: "人均提现",
+                options: [],
+                display: true,
+                tips: "兑换金额/DAU",
+            },
+            user_cont_per_user: {
+                name: "人均贡献",
+                options: [],
+                display: true,
+                tips: "ARPU%(登录用户)-人均提现",
+            },
+            cost_per_user: {
+                name: "投放新增用户成本/人",
+                options: [],
+                tips: "投放消耗/当天注册人数 也就是分渠道报表里面的新增注册成本",
+            },
         },
         list: <any>[],
         columnKeys: <any>[],
@@ -163,20 +308,19 @@ export default class StatisticPlatDaysDateProxy extends AbstractProxy implements
     /**设置表头数据 */
     setTableColumns(data: any) {
         Object.assign(this.tableData.columns, data);
-        
+
         let plat_id_options_keys = Object.keys(this.tableData.columns.plat_id.options);
 
         //将平台中的  0 的系统 删除掉
         const newOptions = <any>{};
         for (let index = 0; index < plat_id_options_keys.length; index++) {
             const element = plat_id_options_keys[index];
-            if (element == 0 || element == "0")
-            {
+            if (element == 0 || element == "0") {
                 continue;
             }
             newOptions[element] = this.tableData.columns.plat_id.options[element];
         }
-        this.tableData.columns.plat_id.options =  newOptions;
+        this.tableData.columns.plat_id.options = newOptions;
         plat_id_options_keys = Object.keys(this.tableData.columns.plat_id.options);
 
         this.tableData.columnKeys = Object.keys(this.tableData.columns);
@@ -225,6 +369,22 @@ export default class StatisticPlatDaysDateProxy extends AbstractProxy implements
         recharge_seep_rate: "",
         arpu_rate: "",
         arppu_rate: "",
+        rech_amt: "",
+        new_reg_rech_total: "",
+        new_reg_first_rech_count: "",
+        net_rech: "",
+        exch_amt: "",
+        new_exch_count: "",
+        p_user_rech: "",
+        p_rech_per_user: "",
+        dau: "",
+        new_user_arpu: "",
+        new_user_arppu: "",
+        d_exch_amt_per_user: "",
+        d_user_cont_per_user: "",
+        d_exch_amt_per_dau: "",
+        user_cont_per_user: "",
+        cost_per_user: "",
     };
     /**表格数据 */
     setTableData(data: any) {
@@ -263,6 +423,22 @@ export default class StatisticPlatDaysDateProxy extends AbstractProxy implements
         this.summaryData["recharge_seep_rate"] = data.summary.recharge_seep_rate;
         this.summaryData["arpu_rate"] = data.summary.arpu_rate;
         this.summaryData["arppu_rate"] = data.summary.arppu_rate;
+        this.summaryData["rech_amt"] = data.summary.rech_amt;
+        this.summaryData["new_reg_rech_total"] = data.summary.new_reg_rech_total;
+        this.summaryData["new_reg_first_rech_count"] = data.summary.new_reg_first_rech_count;
+        this.summaryData["net_rech"] = data.summary.net_rech;
+        this.summaryData["exch_amt"] = data.summary.exch_amt;
+        this.summaryData["new_exch_count"] = data.summary.new_exch_count;
+        this.summaryData["p_user_rech"] = data.summary.p_user_rech;
+        this.summaryData["p_rech_per_user"] = data.summary.p_rech_per_user;
+        this.summaryData["dau"] = data.summary.dau;
+        this.summaryData["new_user_arppu"] = data.summary.new_user_arppu;
+        this.summaryData["d_exch_amt_per_user"] = data.summary.d_exch_amt_per_user;
+        this.summaryData["d_user_cont_per_user"] = data.summary.d_user_cont_per_user;
+        this.summaryData["d_exch_amt_per_dau"] = data.summary.d_exch_amt_per_dau;
+        this.summaryData["user_cont_per_user"] = data.summary.user_cont_per_user;
+        this.summaryData["new_user_arpu"] = data.summary.new_user_arpu;
+        this.summaryData["cost_per_user"] = data.summary.cost_per_user;
         // 把summaryData 插入第一笔
         this.tableData.list.splice(0, 0, this.summaryData);
     }
