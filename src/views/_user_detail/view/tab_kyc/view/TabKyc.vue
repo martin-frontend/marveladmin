b79fd2537375433adb7c011ad06a4f1701a148eb
adam <adam@gmail.com>
2024-01-17 02:47:08 +0000

    H5 6159

---
+++ src/views/_user_detail/view/tab_kyc/view/TabKyc.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="content">
+        <TabKycBody />
+    </div>
+</template>
+
+<script lang="ts">
+import AbstractView from "@/core/abstract/AbstractView";
+import { Component } from "vue-property-decorator";
+import { checkUnique, unique } from "@/core/global/Permission";
+import TabUserInfoProxy from "../../tab_user_info/proxy/TabUserInfoProxy";
+import { getProxy } from "@/views/_user_detail/PageSetting";
+import GlobalVar from "@/core/global/GlobalVar";
+import TabKycBody from "../view/TabKycBody.vue";
+
+@Component({
+    components: {
+        TabKycBody,
+    },
+})
+export default class TabBet extends AbstractView {
+    //权限标识
+    private unique = unique;
+    private checkUnique = checkUnique;
+    //网络状态
+    private net_status = GlobalVar.net_status;
+    // proxy
+    myProxy: TabUserInfoProxy = getProxy(TabUserInfoProxy);
+
+    destroyed() {
+        super.destroyed();
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/common.scss";
+</style>
